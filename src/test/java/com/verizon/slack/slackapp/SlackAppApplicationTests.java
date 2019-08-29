70706a585a2067603658b1dc8bd954f4a9914862
Shubham Maity <shubhammaity89@gmail.com>
2019-08-29 11:52:20 +0530

    initial commit

---
+++ src/test/java/com/verizon/slack/slackapp/SlackAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.verizon.slack.slackapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SlackAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
