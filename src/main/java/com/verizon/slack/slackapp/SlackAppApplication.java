de111a904acf09bd277bbad9fa720a9baf10a858
saranteja7 <saranteja.me@gmail.com>
2021-07-05 09:51:55 +0530

    Revert "New Change"
    This reverts commit 51328b84
    
    (cherry picked from commit 2a390f964575bc81eb09f6291c39f7866883b39e)

--- src/main/java/com/verizon/slack/slackapp/SlackAppApplication.java
+++ src/main/java/com/verizon/slack/slackapp/SlackAppApplication.java
@@ -8,7 +8,7 @@ public class SlackAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SlackAppApplication.class, args);
-		System.out.println("Hello World New");
+		System.out.println("Hello World");
 	}
 
 }
